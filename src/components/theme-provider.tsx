f72d98245f3bf28995762386fa7dcf9e83b04761
dylexBenji <benjichukwudile@gmail.com>
2025-04-02 20:00:01 +0100

    themeprovider fix error

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,26 @@
 'use client'
+import React from 'react'
+import { ThemeProvider } from "next-themes";
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+const ThemeModeProvider = ({children}: any) => {
+  return (
+      <ThemeProvider attribute='class' enableSystem>
+          {children}
+    </ThemeProvider>
+  )
 }
+
+export default ThemeModeProvider
+
+// 'use client'
+
+// import * as React from 'react'
+// import {
+//   ThemeProvider as NextThemesProvider,
+//   type ThemeProviderProps,
+// } from 'next-themes'
+
+// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+// }
